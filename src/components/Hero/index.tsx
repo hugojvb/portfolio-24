40e86a3958c13f058555a2e271eec0c94a9fc280
Hugo Batista <hugojvb20@gmail.com>
2023-05-14 22:04:23 +0100

    Added hero simple component

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,31 @@
+import "./styles.scss";
+
+export default function Hero() {
+  return (
+    <div class="hero">
+      <div class="hero-container">
+        <div class="hero-container__text">
+          <h2>Hello there,</h2>
+          <h1>
+            I'm <span>Hugo</span>
+          </h1>
+          <h2>And I love what I do.</h2>
+          <h3>Front-end Developer</h3>
+          <button class="hero-container__text--button">
+            <a href="#contact">
+              &#x260E; Contact me
+            </a>
+          </button>
+        </div>
+        <div class="hero-container__image">
+          <img
+            src="https://avatars.githubusercontent.com/u/62519943?v=5"
+            alt="Hugo"
+            elementtiming="Hero image"
+            fetchpriority="high"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
