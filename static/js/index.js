8ecb6e094ca209406f736d925a47d0cfc68946d3
hugojvb <hugojvb20@gmail.com>
2023-12-22 23:38:06 +0000

    Added localStorage storage for dark mode

--- static/js/index.js
+++ static/js/index.js
@@ -2,14 +2,23 @@ function toggleDarkMode() {
     const element = document.querySelector("html");
 
     element.classList.toggle("dark");
+    if (!element.classList.contains("dark")) {
+      return;
+    }
+
+    localStorage.setItem("darkMode", "true");
 }
 
 function getDarkMode() {
+  if (localStorage.getItem("darkMode") === "true") {
+    element.classList.add("dark");
+
+    return;
+  }
+
   if (!window.matchMedia || !window.matchMedia('(prefers-color-scheme: dark)').matches) {
     return;
   }
 
-  const element = document.querySelector("html");
-
-  element.classList.add("dark");
+  document.querySelector("html").classList.add("dark");
 }
